c1cddc89def0231b4464816bc15543e682fc86c9
Bishnu Bhattarai <bishnusyangja@gmail.com>
2019-08-15 23:19:15 +0545

    gist code answer completed

--- gist_code_ans.py
+++ gist_code_ans.py
@@ -1,15 +1,15 @@
-from dateutil.relativedelta import relativedelta
+# from dateutil.relativedelta import relativedelta
 from rest_framework import viewsets
 from rest_framework.response import *
 import json
 
 
 # these two classes are created just to avoid warning message in python for my personal comfortability:
-class UserSerializer():
+class UserSerializer(object):
     pass
 
 
-class User():
+class User(object):
     id = None
     pass
 
@@ -45,13 +45,16 @@ any changes.
 """
 
 
+
+
+
+
+
+
+
+
 """
 Answer of the above code starts from here
-
-
-
-
-
 
 """
 
@@ -87,26 +90,59 @@ class LoginRequiredMiddleware(MyMiddleware):
         
         
 # views.py
-class UsersViewSet(viewsets.ModelViewSet):
-    serializer_class = UserSerializer
-    queryset = User.objects.none()
-    # by default no users are available according to permission different filters can
-    # be used in queryset and logics are written in get_queryset method
 
-    def create(self, request):
-        # authentication is checked in LoginRequiredMiddleware so no authentication is required
-        if not request.user.is_authenticated():
-            return Response("You should be authenticated")
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.none() # by default no data is passed
+
+    ''' authentication is checked in LoginRequiredMiddleware so no authentication is not required in
+        this view
         
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            comment = request.POST["comment"]
-            user = User.objects.create(
-                email=serializer.validated_data['email'],
-                password=serializer.validated_data['password'],
-                comment=comment)
-            response = {'code': 200, 'success': True, 'id': user.id}
-            return Response(response)
-        else:
-            print ('error on the creation')
-            return Response('error')
+        user create and validation and other actions with data is performed in serializer so all the actions
+        are moved in serializer class
+        
+        no need to write create method by default viewsets class does it. Viewsets reads data from request and
+        send it to serializer and response accordingly as validation. If any validation fails ValidationError
+        is raised and viewsets handle the exception and sends error message with 400 status code.
+        '''
+    
+    def get_queryset(self):
+        # filtered data is passed according to user type
+        # for example all user list is available only for user.is_staff type
+        # we can check request.user.is_staff and filter accordingly
+        return User.objects.all()
+
+        
+        
+# serializer.py
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = ('email', 'comment', 'password', 'confirm_password', )
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user   = self.context.get('user', None)
+        # requested account if needed we can pass from context
+
+        for field in self.fields.values():
+            field.error_messages.update({'required': '"{fieldname}"  is required'.format(fieldname=field.label),
+                                     'blank': '"{fieldname}" is not allowed blank'.format(fieldname=field.label)})
+            
+    def is_passwd_confirmed(self, password, confirm_password):
+        return password == confirm_password
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', '')
+        confirm_password = validated_data.pop('confirm_password', '')
+        if not self.is_passwd_confirmed(password, confirm_password):
+            raise serializers.ValidationError('password confirmation failed. password and confirm_password did not match')
+        
+        instance = super().create(validated_data)
+        instance.set_password(password)
+        instance.save()
+        return instance
